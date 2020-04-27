b7b01a1d5534b81feccff9b30dcc0ffbd7295453
Justin Bender <jrkbender@gmail.com>
2020-04-27 09:56:14 -0400

    saving first commit

---
+++ pythonLoops.py
@@ -0,0 +1,43 @@
+dog_breeds = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+
+for breed in dog_breeds:
+  print(breed)
+
+board_games = ['Settlers of Catan', 'Carcassone', 'Power Grid', 'Agricola', 'Scrabble']
+
+sport_games = ['football', 'football - American', 'hockey', 'baseball', 'cricket']
+
+for game in board_games:
+  print(game)
+  
+for sport in sport_games:
+  print(sport)
+
+promise = "I will not chew gum in class"
+
+for i in range(5):
+  print(promise)
+
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in students_period_A:
+  print(student)
+  students_period_B.append(student)
+
+dog_breeds_available_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+dog_breed_I_want = 'dalmatian'
+
+for dog in dog_breeds_available_for_adoption:
+  print(dog)
+  if dog == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+  if age < 21:
+    continue
+  print(age)
+
